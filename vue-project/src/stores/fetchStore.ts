d4e37260a5434c9896c6af4ca11ea5e94dc28098
abdulmajid <abdulmajid08180881@gmail.com>
2025-03-05 00:57:08 +0700

    feature: added state management pinia; integrate api newsApi with axios

---
+++ vue-project/src/stores/fetchStore.ts
@@ -0,0 +1,51 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+import axios from 'axios'
+
+// code typescript
+interface NewsSource {
+  name: string
+}
+
+interface NewsData {
+  title: string
+  description: string
+  newsUrl: string
+  imageNews: string
+  content: string
+  published: string
+  sources: NewsSource
+}
+
+interface BookmarkData {
+  title: string
+  imageUrl: string
+  linkUrl: string
+}
+
+export const useFetchStore = defineStore('fetch', () => {
+  // state
+  const baseURL = ref<string>(import.meta.env.VITE_API_BASE_URL)
+  const apiKey = ref<string>(import.meta.env.VITE_API_KEY)
+  const listDataNews = ref<NewsData[]>([])
+  const isLoading = ref<boolean>(true)
+
+  // fetch api news
+  const fetchListNews = async () => {
+    try {
+      isLoading.value = true
+      const response = await axios.get(`${baseURL.value}?apiKey=${apiKey.value}&country=us`)
+      listDataNews.value = response.data.articles
+    } catch (error) {
+      console.error(error)
+    } finally {
+      isLoading.value = false
+    }
+  }
+
+  return {
+    isLoading,
+    listDataNews,
+    fetchListNews,
+  }
+})
--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,9 +1,23 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { useFetchStore } from '../stores/fetchStore.ts'
+import { onMounted } from 'vue'
+import { storeToRefs } from 'pinia'
+
+const fetchStore = useFetchStore()
+const { isLoading, listDataNews } = storeToRefs(fetchStore)
+
+// function
+const { fetchListNews } = useFetchStore()
+
+onMounted(() => {
+  fetchListNews()
+})
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>welcome to home</h1>
+  <h2>List Data:</h2>
+  <br />
+  <div v-if="isLoading">Loading....</div>
+  <div v-else>{{ listDataNews }}</div>
 </template>
